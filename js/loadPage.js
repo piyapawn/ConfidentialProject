bbd326ed880cf7c51be95f1a421a47f52ba5db87
Punnaphat Phiamtanapuwanon <punnaphat.zaza@gmail.com>
2023-04-22 03:58:23 +0700

    update index.html & loadPage.js

--- js/loadPage.js
+++ js/loadPage.js
@@ -1,19 +1,24 @@
-function loadPage(nextPageId, prevPageId) {
-    let nextPage = document.getElementById(nextPageId)
+function loadPage(prevPageId, nextPageId) {
     let prevPage = document.getElementById(prevPageId)
+    let nextPage = document.getElementById(nextPageId)
 
-    console.log("next page: "+nextPageId+"| prev page: "+prevPageId)
+    console.log("prev page: "+prevPageId+"| next page: "+nextPageId)
 
-    if(nextPageId === "intro3" && prevPageId === "intro2") {
+    document.documentElement.style.setProperty('--cursor', 'auto')
+
+    if(prevPageId === "intro1" && nextPageId === "intro2") {
         setTimeout(() => {
-            nextPage.style.zIndex = 1
-            nextPage.style.opacity = 1
             nextPage.style.display = "inline-block"
-            
-            prevPage.style.zIndex = -999
+        }, 5600)
+
+        setTimeout(() => {
             prevPage.style.opacity = 0
             prevPage.style.display = "none"
-        }, 4000);
+        }, 5800);
+
+        setTimeout(() => {
+            nextPage.style.opacity = 1
+        }, 6000);
     }
     else {
         nextPage.style.zIndex = 1
@@ -27,18 +32,28 @@ function loadPage(nextPageId, prevPageId) {
 }
 
 function activeFade() {
-    let light = document.getElementsByClassName("light")
-    light[0].style.background = "black"
-    light[0].style.animation = "blackToTransparent 2s ease-in alternate"
+    let firstText = document.getElementById("firstText")
+    let accessButton = document.getElementById("accessButton")
+    let intro1Pic = document.getElementById("intro1-pic")
+    let intro1Gif = document.getElementById("intro1-gif")
+    let light = document.getElementById("light")
+
+    firstText.style.display = "none"
+    accessButton.style.display = "none"
+
+    light.style.background = "white"
+    light.style.animation = "whiteToTransparent 2s ease-in alternate"
     setTimeout(() => {
-        light[0].style.background = "transparent"
+        light.style.background = "transparent"
+        intro1Pic.style.display = "none"
+        intro1Gif.style.display = "block"
     }, 2000);
 
-    let fade = document.getElementById("fadingEffect")
-    fade.style.zIndex = 10
-    fade.style.animation = "fadeBlack 4s ease-in alternate"
-    fade.style.animationDelay = "2s"
-    setTimeout(() => {
-        fade.style.display = "none"
-    }, 6000);
+    // let fade = document.getElementById("fadingEffect")
+    // fade.style.zIndex = 10
+    // fade.style.animation = "fadeBlack 4s ease-in alternate"
+    // fade.style.animationDelay = "2s"
+    // setTimeout(() => {
+    //     fade.style.display = "none"
+    // }, 6000);
 }
