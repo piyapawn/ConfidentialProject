8cb0f1aa901a2a60f3b8421641830be72f7a656a
Punnaphat Phiamtanapuwanon <punnaphat.zaza@gmail.com>
2023-04-25 02:43:36 +0700

    Update loadHTML

--- js/loadHTML.js
+++ js/loadHTML.js
@@ -1,27 +1,28 @@
 function loadHTML(htmlPath, timeout) {
     console.log("Load")
+    document.getElementsByTagName("body")[0].style.animation = 'fade-out 0.5s'
     setTimeout(() => {
         window.location.replace(htmlPath);
     }, timeout);
 }
 
-function loadHTML2(id, fileName) {
-    console.log("Load")
+// function loadHTML2(id, fileName) {
+//     console.log("Load")
     
-    let xhttp;
-    let element = document.getElementById(id)
-    let file = fileName
+//     let xhttp;
+//     let element = document.getElementById(id)
+//     let file = fileName
 
-    if(file) {
-        xhttp = new XMLHttpRequest()
-        xhttp.onreadystatechange = function() {
-            if(this.readyState == 4) {
-                if (this.status == 200) { element.innerHTML = this.responseText }
-                if (this.status == 404) { element.innerHTML = "<h1>Page not found.</h1>" }
-            }
-        }
-        xhttp.open("GET", `${file}`,true)
-        xhttp.send()
-        return
-    }
-}
+//     if(file) {
+//         xhttp = new XMLHttpRequest()
+//         xhttp.onreadystatechange = function() {
+//             if(this.readyState == 4) {
+//                 if (this.status == 200) { element.innerHTML = this.responseText }
+//                 if (this.status == 404) { element.innerHTML = "<h1>Page not found.</h1>" }
+//             }
+//         }
+//         xhttp.open("GET", `${file}`,true)
+//         xhttp.send()
+//         return
+//     }
+// }
