02b4a3902f9e74fed38fc7713b2c2a46d5a8d8c2
piyapawn <clasperqueen@gmail.com>
2023-06-01 23:30:50 +0700

    Fixed zone02
    -user cannot select the same choice multiple time
    -user cannot open many information at once
    -when selected and styling to the choice

--- zone02/game/agent.js
+++ zone02/game/agent.js
@@ -17,9 +17,9 @@ function addQuant(quantityId, answer, sendBtId, buttonId, userBtId) {
     
     // disable choose button of this user, prevent repeat submit
     disableBt(buttonId)
+    document.getElementById(buttonId).style.border = "solid 0.5px #0D3BFC"
     // If user is choosen, styling navy border
-    userButton.style.border = "solid 2px #0D3BFC";
-
+    userButton.style.border = "solid 3px #0D3BFC";
 
     storing(answer);
     if(quantityId === 'quantity1' && quantity<5) {
@@ -38,9 +38,10 @@ function addQuant(quantityId, answer, sendBtId, buttonId, userBtId) {
 
 // end this zone, when task completed
 function endIt() {
-    document.getElementById('endingZone').style.zIndex = 4
-    document.getElementById('endingZone').style.animation = 'slideIn 1s 1'
-    document.getElementById('endingZone').style.animationFillMode = 'forwards'
+    let endEle = document.getElementById('endingZone')
+    endEle.style.zIndex = 4
+    endEle.style.animation = 'slideIn 1s 1'
+    endEle.style.animationFillMode = 'forwards'
 }
 
 // sending the answer of selected user
@@ -95,10 +96,50 @@ function toggle(eleId) {
     document.getElementById(eleId).classList.toggle('showRequire');
 }
 
+function showUserData(userId) {
+    let userEle = document.getElementById(userId)
+    toggle(userId);
+    userEle.style.animation = "fadeClear 1s 1"
+    userEle.style.animationFillMode = 'forwards'
+}
+
+// this function needs client ID and user ID that u want to show the information detail
+function showUserDetail(clientId, interestId, currentBtId) {
+    const showDataBtSet1 = ['dataBt11', 'dataBt12', 'dataBt13', 'dataBt14', 'dataBt15', 'dataBt16', 'dataBt17'];
+    const showDataBtSet2 = ['dataBt21', 'dataBt22', 'dataBt23', 'dataBt24', 'dataBt25'];
+    const showDataBtSet3 = ['dataBt31', 'dataBt32', 'dataBt33', 'dataBt34', 'dataBt35'];
+
+    toggle(interestId)
+
+    let usingShowDataSet
+    switch(clientId) {
+        case 1:
+            usingShowDataSet = showDataBtSet1;
+            break;
+        case 2:
+            usingShowDataSet = showDataBtSet2;
+            break;
+        case 3:
+            usingShowDataSet = showDataBtSet3;
+            break;
+    }
+
+    for (eachButton in usingShowDataSet) {
+        if (usingShowDataSet[eachButton] != currentBtId) {
+            console.log('thisButton', usingShowDataSet[eachButton])
+            disableBt(usingShowDataSet[eachButton])
+        }
+    }
+}
+
 function showRequirement(questId, otherBt1, otherBt2, userId) {
     let areaProv = document.getElementById('areaText')
+    let usersEle = document.getElementById(userId)
     toggle(questId);
     toggle(userId);
+    // set opacity to be 100% after animation end
+    usersEle.style.animation = "fadeClear 1s 1"
+    usersEle.style.animationFillMode = 'forwards'
     disableBt(otherBt1);
     disableBt(otherBt2);
     if(questId == "quest01" || questId == "quest02") {
@@ -106,20 +147,20 @@ function showRequirement(questId, otherBt1, otherBt2, userId) {
     }else if(questId == "quest03") {
         areaProv.innerHTML = "area : Chiang mai";
     }
-    console.log('here is id : ', questId, '  ', areaProv)
 }
 
-function showhideInfo(infoId, showorhide, bt) { 
+function showhideInfo(infoId, showorhide, bt) {
+    let informationElement = document.getElementById(infoId)
     if(showorhide) {
-        document.getElementById(infoId).style.height = '100%'
-        document.getElementById(infoId).style.width = '100%'
-        document.getElementById(infoId).style.transition = 'height 0.3s, width 0.5s'
+        informationElement.style.height = '100%'
+        informationElement.style.width = '100%'
+        informationElement.style.transition = 'height 0.3s, width 0.5s'
         document.getElementById(bt).style.zIndex = 3
     }
     else {
-        document.getElementById(infoId).style.height = '0%'
-        document.getElementById(infoId).style.width = '0%'
-        document.getElementById(infoId).style.transition = 'height 0.3s, width 0.5s'
+        informationElement.style.height = '0%'
+        informationElement.style.width = '0%'
+        informationElement.style.transition = 'height 0.3s, width 0.5s'
         document.getElementById(bt).style.zIndex = -3
     }
 }
