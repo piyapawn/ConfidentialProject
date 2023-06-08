19cdbce801eb63a8c9e0d08d395006207eabff1e
Punnaphat Phiamtanapuwanon <punnaphat.zaza@gmail.com>
2023-06-09 05:51:09 +0700

    New DatabaseTour Update

--- js/databaseTour.js
+++ js/databaseTour.js
@@ -1,23 +1,211 @@
+// Database Box Mock Info
+let databaseAddressNumMockCol1 = [
+    '38.169.83.225',
+    '216.255.96.87',
+    '192.61.200.76',
+    '41.53.3.48',
+    '32.153.3.46',
+    '163.235.12.53',
+    '87.213.46.181',
+    '31.15.143.48',
+    '133.58.64.56',
+    '48.36.255.48',
+    '25.43.153.5',
+    '146.34.154.143',
+    '168.44.183.3',
+    '6.5.40.83',
+    '8.6.54.165',
+    '144.185.245.4',
+    '246.87.46.158',
+    '79.35.179.255',
+    '3.151.120.51',
+    '189.179.80.164',
+    '8.154.168.4',
+    '31.103.251.238',
+    '231.20.215.84',
+    '35.101.51.46',
+    '55.1.25.48'
+]
+let databaseAddressNumMockCol2 = [
+    '64.53.213.153',
+    '255.21.165.51',
+    '86.15.5.143',
+    '153.51.53.34',
+    '56.54.54.154',
+    '43.43.54.165',
+    '3.45.54.442',
+    '24.153.26.53',
+    '31.55.5.121',
+    '96.51.3.13',
+    '33.255.18.94',
+    '65.16.4.62',
+    '25.45.215.1',
+    '187.197.9.54',
+    '13.141.6.254',
+    '158.236.35.34',
+    '164.8.47.161',
+    '78.241.66.14',
+    '248.51.4.245',
+    '5.44.6.5',
+    '156.145.24.96',
+    '51.2.245.20',
+    '10.2.51.146',
+    '196.198.4.54',
+    '168.9.176.185',
+    '156.4.30.6',
+    '70.253.136.44',
+    '231.12.51.104',
+    '3.51.132.60',
+    '251.201.54.64'
+]
+
+let databaseAddressNumMockCol3 = [
+    '16.48.154.34',
+    '5.4.155.8',
+    '87.223.46.181',
+    '12.153.233.221',
+    '163.51.25.231',
+    '75.32.83.4',
+    '6.155.68.92',
+    '35.4.51.246',
+    '162.112.45.64',
+    '164.141.63.64',
+    '96.46.164.116',
+    '85.45.131.151',
+    '46.45.115.84',
+    '15.41.131.251',
+    '44.54.65.4',
+    '3.61.24.140',
+    '35.6.1.51',
+    '251.1.16.46',
+    '156.148.154',
+    '46.5.164.164',
+    '7.5.216.168',
+    '156.14.186.245',
+    '16.151.4.65',
+    '251.161.245.4',
+    '1.231.253.168',
+    '50.2.251.36',
+    '103.158.64.68',
+    '184.176.4.248',
+    '44.72.56.144',
+    '86.1.244.64',
+    '4.251.4.206',
+    '131.2.35.53',
+    '210.25.148.64',
+    '30.241.156.46',
+    '235.146.131.2',
+    '66.48.86.40'
+]
+
+let databaseAddressNumMockCol4 = [
+    '26.5.146.6',
+    '161.31.21.35',
+    '95.98.155.42',
+    '145.49.43.66',
+    '2.65.4.65',
+    '123.46.4.61',
+    '165.136.51.35',
+    '58.169.83.225',
+    '231.13.15.184',
+    '68.176,184.85',
+    '174.115.3.151',
+    '95.246.87.99',
+    '68.246.176.95',
+    '78.162.47.85',
+    '7.189.6.254',
+    '165.46.63.16',
+    '154.69.5.102',
+    '251.10.135.145',
+    '56.143.204.23',
+    '5.30.12.146',
+    '235.201.43.165',
+    '98.76.4.158',
+    '196.184.148.9',
+    '178.6.58.65'
+]
+
+let databaseAddressNumMockCol5 = [
+    '56.146.151.36',
+    '165.3.35.215',
+    '23.244.3.54',
+    '156.6.5.16',
+    '5.4.68.91',
+    '97.4.62.56',
+    '48.3.59.164',
+    '153.143.131.51',
+    '31.16.248.246',
+    '189.167.145.6',
+    '49.26.145.184',
+    '167.9.96.58',
+    '25.6.84.103',
+    '4.53.40.182',
+    '204.56.158.31',
+    '154.68.5.102',
+    '18.16.145.126',
+    '246.158.245.30',
+    '136.154.26.41',
+    '125.26.140.65',
+    '4.86.45.21',
+    '156.246.40.61',
+    '.17.6.84.120',
+    '5.4.168.248',
+    '146.184.7.80'
+]
+
+let databaseAddressNumMockCol6 = [
+    '32.51.12.113',
+    '127.255.255.255',
+    '3.56.84.92',
+    '113.231.121.3',
+    '96.253.67.9',
+    '224.35.167.168',
+    '9.65.83.251',
+    '64.234.189.178',
+    '36.59.88.199',
+    '63.99.34.9',
+    '254.16.80.255',
+    '201.255.74',
+    '52.201.84.51',
+    '10.1.56.246',
+    '60.186.247.196',
+    '87.60.50.114',
+    '164.8.49.68',
+    '90.154.61.84',
+    '187.2.68.40',
+    '14.75.4.84',
+    '75.136.40.186',
+    '51.162.204.36',
+    '3.184.26.85',
+    '156.141.164.85',
+    '40.21.245.102',
+    '24.165.28.186',
+    '178.196.4.154',
+    '8.14.106.59',
+    '106.46.81.51',
+    '54.134.26.54',
+    '13.255.63.61',
+    '45.31.201.61',
+    '146.251.153.3',
+    '135.161.205'
+]
+
 // Define Variables
 let imageBox
-let dot
+let yellowDot
+
 let image
 let product
 let infoBox
 
-let subnetButtons
-let subnetTopicAreas
-let subnetTopics
-let subnetLargeTopicAreas
-let subnetLargeTopics
-let subnetContentAreas
+// CSS Root Variables
+let gridVw = 2.565
+let gridPx
 
-let body
-let bg
+// Window
+let windowHeight
+
 let blueBGTexts
-let subNetwork
-
-let subWWW
 
 // Declare Variables (after Window Load)
 function windowLoad() {
@@ -28,18 +216,20 @@ function windowLoad() {
 
     document.getElementsByTagName("body")[0].style.animation = 'fade-in 0.5s'
 
-    body = document.getElementsByTagName('body')
-    console.log('Body : ', body[0])
-
-    bg = document.getElementById('bg')
-
     blueBGTexts = document.getElementsByClassName('blue-bg')
-    subNetwork = document.getElementsByClassName('sub-network')
-    for(let i of blueBGTexts) {
-        console.log('blueBGTexts Text : ', i.offsetTop)
-    }
 
-    subWWW = document.getElementsByClassName('sub-www')
+    // Window
+    windowWidth = window.innerWidth
+    windowHeight = window.innerHeight
+
+    gridPx = (windowWidth * gridVw) / 100
+
+    createDatabaseBoxMock(1, databaseAddressNumMockCol1)
+    createDatabaseBoxMock(2, databaseAddressNumMockCol2)
+    createDatabaseBoxMock(3, databaseAddressNumMockCol3)
+    createDatabaseBoxMock(4, databaseAddressNumMockCol4)
+    createDatabaseBoxMock(5, databaseAddressNumMockCol5)
+    createDatabaseBoxMock(6, databaseAddressNumMockCol6)
 
     setTimeout(() => {
         blueBGTexts[0].style.opacity = 1
@@ -49,25 +239,42 @@ function windowLoad() {
 let checkArray = Array(7).fill(true)
 function databaseBoxClick(id) {
     imageBox = document.getElementById(`img-box-${id}`)
-    dot = document.getElementById(`dot-${id}`)
-    console.log('Check Array: ', checkArray)
+    yellowDot = document.getElementById(`yellow-dot-${id}`)
     
     if(checkArray[id-1]) {
-        dot.style.animationPlayState = 'paused'
-        dot.style.background = 'var(--yellow)'
+        yellowDot.style.animationPlayState = 'paused'
+        yellowDot.style.background = 'var(--yellow)'
         imageBox.style.display = 'block'
         imageBox.style.animation = 'fade-in 0.5s'
         imageBox.style.opacity = 1
         checkArray[id-1] = false
     }
     else {
-        dot.style.animationPlayState = 'running'
+        yellowDot.style.animationPlayState = 'running'
         imageBox.style.animation = 'fade-out 0.5s'
         imageBox.style.opacity = 0
         setTimeout(() => {
             imageBox.style.display = 'none'
         }, 500)
         checkArray[id-1] = true
+    }
+}
+
+function createDatabaseBoxMock(colNum, numMockCol) {
+    let databaseContentArea = document.getElementById('database-content-area')
+
+    for(let i = 1; i <= numMockCol.length; i++) {
+        databaseContentArea.innerHTML += `
+        <!-- Database Box Mock -->
+        <div class="database-box-mock db-mock-column-${colNum} background-white" id="database-box-mock-col${colNum}-${i}">
+            <div class="yellow-dot-box">
+                <div class="yellow-dot-mock background-yellow"></div>
+            </div>
+            <div class="address-number-box">
+                <p class="address-number-text font-dbHelvethaica">${numMockCol[i-1]}</p>
+            </div>
+        </div>
+        `
     }
 }
 
@@ -91,104 +298,102 @@ function imageNotHover(id) {
     infoBox.style.display = 'none'
 }
 
+function scrollAnimation() {
+    let bodyScroll = document.documentElement.scrollTop
+
+    // let scrollText = document.getElementById('scroll-text-show')
+    // scrollText.innerHTML = Math.floor(bodyScroll)
+
+    let subnetBox = document.getElementsByClassName('sub-network')
+
+    let subwwwBox = document.getElementsByClassName('sub-www')
+
+    // Subnet Offset
+    let subnetContentAreaTop = document.getElementById('subnet-content-area-top')
+    let scaOffsetTop = Math.floor(subnetContentAreaTop.offsetTop + gridPx)
+
+    // WWW Offset
+    let wwwContentAreaTop = document.getElementById('www-content-area-top')
+    let wwwcaOffsetTop = Math.floor(wwwContentAreaTop.offsetTop + gridPx)
+
+    boxReveal(subnetBox[0], scaOffsetTop, 2)
+    boxReveal(subnetBox[1], scaOffsetTop, 4)
+    boxReveal(subnetBox[2], scaOffsetTop, 6)
+    boxReveal(subnetBox[3], scaOffsetTop, 8)
+
+    boxReveal(subwwwBox[0], wwwcaOffsetTop, 3)
+    boxReveal(subwwwBox[1], wwwcaOffsetTop, 6)
+    boxReveal(subwwwBox[2], wwwcaOffsetTop, 9)
+
+    let footer = document.getElementById('footer-next-page')
+    let scrollArrow = document.getElementById('scroll-arrow')
+
+    // Footer Reveal
+    if(bodyScroll >= wwwcaOffsetTop + (120 * 12)) {
+        revealElement(footer, 'flex')
+        hideElement(scrollArrow)
+    }
+    else {
+        revealElement(scrollArrow, 'block')
+        hideElement(footer)
+    }
+
+    // Blue Text Reveal
+    for(let i = 1; i < blueBGTexts.length-2; i++) {
+        if(bodyScroll + windowHeight/1.5 >= blueBGTexts[i].offsetTop) {
+            blueBGTexts[i].style.opacity = 1
+        }
+    }
+    if(bodyScroll >= scaOffsetTop) {
+        blueBGTexts[4].style.opacity = 1
+    }
+    if(bodyScroll >= wwwcaOffsetTop) {
+        blueBGTexts[5].style.opacity = 1
+    }
+}
+
+function boxReveal(elementBox, contentAreaOffsetTop, numEach) {
+    let bodyScroll = document.documentElement.scrollTop
+
+    if(bodyScroll >= contentAreaOffsetTop + (120 * numEach)) {
+        elementBox.style.opacity = 1
+        elementBox.style.transform = 'translateY(0%)'
+    }
+    else {
+        elementBox.style.opacity = 0
+        elementBox.style.transform = 'translateY(calc(var(--grid) * 5)'
+    }
+}
+
 function subnetButtonClick() {
-    subnetButtons = document.getElementsByClassName('sub-network')
-    subnetTopicAreas = document.getElementsByClassName('subnet-topic-area')
-    subnetTopics = document.getElementsByClassName('subnet-topic-text')
-    subnetLargeTopicAreas = document.getElementsByClassName('subnet-large-topic-area')
-    subnetContentAreas = document.getElementsByClassName('subnet-content-area')
+    let subnetButtons = document.getElementsByClassName('sub-network')
+    let subnetTopicAreas = document.getElementsByClassName('subnet-topic-area')
+    let subnetTopics = document.getElementsByClassName('subnet-topic-text')
+    let subnetLargeTopicAreas = document.getElementsByClassName('subnet-large-topic-area')
+    let subnetContentBoxAreas = document.getElementsByClassName('subnet-content-box-area')
 
     for(let i = 0; i < subnetButtons.length; i++) {
-        subnetButtons[i].style.height = 'calc(var(--gridHeight) * 15)'
+        subnetButtons[i].style.height = 'calc(var(--grid) * 15)'
 
-        subnetTopicAreas[i].style.height = 'calc(var(--gridHeight) * 10.5)'
+        subnetTopicAreas[i].style.height = 'calc(var(--grid) * 10.5)'
         subnetTopics[i].style.display = 'none'
         subnetLargeTopicAreas[i].style.display = 'flex'
-        subnetContentAreas[i].style.display = 'flex'
+        subnetContentBoxAreas[i].style.display = 'flex'
 
         console.log('Button Click')
     }
 }
 
-function scrollRevealText() {
-    let bodyScroll = document.documentElement.scrollTop
-
-    // let scrollText = document.getElementById('scrollTop-text')
-    // scrollText.innerHTML = 'Scroll: '+ Math.floor(bodyScroll)
-
-    let bgImage = document.getElementById('bg-image')
-
-    let sticky1 = document.getElementsByClassName('sticky-1')
-
-    for(let i = 0; i < subNetwork.length; i++) {
-        let scene = [3550, 3850, 4150, 4450]
-        let count = bodyScroll - scene[i]
-        if(bodyScroll >= scene[i]) {
-            subNetwork[i].style.opacity = (count * 0.01)
-            subNetwork[i].style.transform = 'translateY(0)'
-        }
-        else {
-            subNetwork[i].style.opacity = 0
-            subNetwork[i].style.transform = 'translateY(calc(var(--gridHeight) * 5))'
-        }
-    }
-
-    for(let i = 0; i < subWWW.length; i++) {
-        let scene = [5745, 6045, 6345]
-        let count = bodyScroll - scene[i]
-        if(bodyScroll >= scene[i]) {
-            subWWW[i].style.opacity = (count * 0.01)
-            subWWW[i].style.transform = 'translateY(0)'
-        }
-        else {
-            subWWW[i].style.opacity = 0
-            subWWW[i].style.transform = 'translateY(calc(var(--gridHeight) * 5))'
-        }
-    }
-
-    for(let i = 1; i < blueBGTexts.length-2; i++) {
-        if(bodyScroll+window.innerHeight/1.5 >= blueBGTexts[i].offsetTop) {
-            blueBGTexts[i].style.opacity = 1
-        }
-    }
-    if(bodyScroll >= 3550) {
-        blueBGTexts[4].style.opacity = 1
-    }
-    if(bodyScroll >= 5745) {
-        blueBGTexts[5].style.opacity = 1
-    }
-
-    let footerNextPage = document.getElementsByClassName('footer-next-page')[0]
-    let arrowDown = document.getElementById('scroll-arrow')
-    if(bodyScroll >= 6645) {
-        footerNextPage.style.opacity = 1
-        arrowDown.style.opacity = 0
-    }
-    else {
-        footerNextPage.style.opacity = 0
-        arrowDown.style.opacity = 1
-    }
-
-    // for(let i = 0; i < subNetwork.length; i++) {
-    //     if(bodyScroll+window.innerHeight/1.8 >= subNetwork[i].offsetTop) {
-    //         subNetwork[i].style.opacity = 1
-    //         subNetwork[i].style.transform = 'translateY(0)'
-    //     }
-    // }
+function revealElement(element, display) {
+    element.style.display = display
+    setTimeout(() => {
+        element.style.opacity = 1
+    }, 250)
 }
 
-
-
-
-
-
-// Scroll Magic
-
-// const tween = new TiemlineLite()
-
-// tween.add(
-//     TweenLite.to('.sticky'), 1, {
-
-//     }
-// )
-
+function hideElement(element) {
+    element.style.opacity = 0
+    setTimeout(() => {  
+        element.style.display = 'none'
+    }, 250)
+}
